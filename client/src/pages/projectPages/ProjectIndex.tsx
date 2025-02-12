f4bc3f1d4c8bb8356cf8463dcb86905385f14f86
Faccinetto <bastien.faccinetto@gmail.com>
2025-02-12 14:48:33 +0100

    Page ProjectIndex // CSS Mobile

--- client/src/pages/projectPages/ProjectIndex.tsx
+++ client/src/pages/projectPages/ProjectIndex.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Project_Picture from "../../assets/images/generic_project_image.jpg";
+import Add from "../../assets/images/icons/icon_add.png";
 import "./ProjectIndex.css";
 
 type Project = {
@@ -28,10 +29,11 @@ function ProjectIndex() {
     <section className="projects">
       <h1 id="projects_title">Projets</h1>
       <section className="projects_card_container">
-        <ul>
+        <ul className="project_informations">
           {projects.map((project) => (
-            <li key={project.id}>
-              <Link to={`/project/${project.id}`} className="project_card">
+            <li key={project.id} className="project_card">
+              <Link to={`/project/${project.id}`}>
+                <h2 className="project_title">{project.title}</h2>
                 <img
                   src={
                     project.image
@@ -39,24 +41,40 @@ function ProjectIndex() {
                       : Project_Picture
                   }
                   alt="Project_Picture"
-                  id="project_picture"
+                  className="project_picture"
                 />
-                <div>
-                  <strong>{project.title}</strong>
-                  <p>{project.year}</p>
-                  <p>{project.technologies}</p>
-                  <p>{project.description}</p>
+                <div className="card_informations">
+                  <p className="card_item">
+                    <strong className="item_bold">Année :</strong>{" "}
+                    {project.year}
+                  </p>
+                  <p className="card_item">
+                    <strong className="item_bold">Technologies :</strong>{" "}
+                    {project.technologies}
+                  </p>
+                  <p className="card_item">
+                    <strong className="item_bold">Description :</strong>{" "}
+                    {project.description}
+                  </p>
                 </div>
               </Link>
-              <button type="button">Modifier</button>
-              <button type="button">Supprimer</button>
+              <div className="action_buttons">
+                <button type="button" className="action_button">
+                  Modifier
+                </button>
+                <button type="button" className="action_button">
+                  Supprimer
+                </button>
+              </div>
             </li>
           ))}
         </ul>
       </section>
 
       <Link to="/add_project">
-        <button type="button">+</button>
+        <button type="button" id="add_button">
+          <img src={Add} alt="add_project" id="add_project" />
+        </button>
       </Link>
     </section>
   );
