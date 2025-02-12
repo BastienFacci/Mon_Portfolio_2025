f1567ea9a1d9f11b6849ab5aea621347ee205b32
Faccinetto <bastien.faccinetto@gmail.com>
2025-02-12 10:24:32 +0100

    Création tables schéma.sql //fichiers actions et repositoru en BACK // import images, polices

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,21 +1,39 @@
-create table user (
-  id int unsigned primary key auto_increment not null,
-  email varchar(255) not null unique,
-  password varchar(255) not null
+CREATE TABLE user (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, 
+  firstname VARCHAR(50) NOT NULL,
+  lastname VARCHAR(50) NOT NULL, 
+  email VARCHAR(255) NOT NULL UNIQUE, 
+  password VARCHAR(255) NOT NULL
 );
 
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
+CREATE TABLE project (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, 
+  title VARCHAR(100) NOT NULL, 
+  year YEAR NOT NULL, 
+  description TEXT, 
+  technologies VARCHAR(255), 
+  user_id INT UNSIGNED NOT NULL, 
+  FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
 );
 
-insert into user(id, email, password)
-values
-  (1, "jdoe@mail.com", "123456");
+CREATE TABLE job (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, 
+  society VARCHAR(100) NOT NULL, 
+  job_title VARCHAR(50) NOT NULL, 
+  start_date DATE NOT NULL, 
+  end_date DATE, 
+  description TEXT, 
+  user_id INT UNSIGNED NOT NULL, 
+  FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
+);
 
-insert into item(id, title, user_id)
-values
-  (1, "Stuff", 1),
-  (2, "Doodads", 1);
+
+INSERT INTO user (firstname, lastname, email, password) 
+VALUES 
+  ("Bastien", "Faccinetto", "bastien.faccinetto@gmail.com", "toto");
+
+INSERT INTO project (title, year, description, technologies, user_id) 
+VALUES 
+  ("LABS Society", 2024, "Trombinoscope d’une société fictive, conçu avec un design sobre et épuré pour faciliter la visualisation des équipes et des collaborateurs.", "HTML, CSS, Javascript", 1),
+("Rotten Totomatoes", 2024, "Application web de référencement de séries américaines, inspirée de sites comme Rotten Tomatoes et Allociné. Elle permet de découvrir, évaluer et consulter des informations détaillées sur diverses séries.", "React, TypeScript, Node.js,  Express, Vite.js, JavaScript, HTML, CSS ", 1);
+ 
