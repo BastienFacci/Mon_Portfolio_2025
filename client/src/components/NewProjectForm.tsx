70f8043c7536012dfe79ae4e14db3e4fd9c64440
Faccinetto <bastien.faccinetto@gmail.com>
2025-02-12 17:32:03 +0100

    C'est super

--- client/src/components/NewProjectForm.tsx
+++ client/src/components/NewProjectForm.tsx
@@ -1,44 +1,88 @@
+import type { ReactNode } from "react";
 import "./NewProjectForm.css";
 
-function NewProjectForm() {
+type ProjectType = {
+  title: string;
+  year: string;
+  technologies: string;
+  description: string;
+};
+
+interface NextProjectType {
+  children?: ReactNode; // contenu optionnel pour le bouton dynamique
+  defaultValue: ProjectType; // valeurs par défaut du formulaire
+  onSubmit: (project: FormData) => void; // fonction appelée lors de la soumission d'un projet
+}
+
+function NextProjectForm({
+  children,
+  defaultValue,
+  onSubmit,
+}: NextProjectType) {
+  // obtention de la date actuelle au format aaaa-mm-dd
+  const today = new Date().toISOString().split("T")[0];
+  const formattedDate = new Date().toLocaleDateString("fr-FR"); // Format de la date au format DD-MM-YYYY
+
   return (
-    <section className="NewProjectForm_container">
-      <form>
-        <label htmlFor="Titre">Titre</label>
+    <section className="post-project-container">
+      <h1 id="subm_project">AJOUTER UN PROJET</h1>
+      <form
+        className="form-project"
+        onSubmit={(event) => {
+          event.preventDefault(); // empêche le rechargement de la page lors de la soumission
+          const formData = new FormData(event.currentTarget);
+          formData.append("date", today);
+          formData.append("dateoftheday", formattedDate); // Ajouter la date formatée
+          onSubmit(formData);
+        }}
+      >
+        <label htmlFor="title">TITRE DU PROJET</label>
         <input
-          className="newprojectform_field"
-          placeholder="Titre"
+          className="form-project-fields"
+          placeholder="TITRE DU PROJET"
           type="text"
           name="title"
+          defaultValue={defaultValue.title}
         />
-        <label htmlFor="Année">Année</label>
+
+        <label htmlFor="year">ANNÉE DE CRÉATION</label>
         <input
-          className="newprojectform_field"
-          placeholder="Année"
-          type="number"
-          min="2024"
-          max={new Date().getFullYear()}
-          onChange={(e) => console.log(e.target.value)}
-          name="title"
+          className="form-project-fields"
+          placeholder="ANNÉE"
+          type="text"
+          name="year"
+          defaultValue={defaultValue.year}
         />
-        <label htmlFor="Technologies">Technologies</label>
+
+        <label htmlFor="technologies">TECHNOLOGIES UTILISÉES</label>
         <input
-          className="newprojectform_field"
-          placeholder="Technologies"
+          className="form-project-fields"
+          placeholder="TECHNOLOGIES"
           type="text"
           name="technologies"
+          defaultValue={defaultValue.technologies}
         />
-        <label htmlFor="Titre">Description</label>
+
+        <label htmlFor="description">DESCRIPTION DU PROJET</label>
         <textarea
-          id="description"
-          placeholder="Description"
-          rows={4}
+          className="form-project-fields"
+          rows={5}
           cols={50}
+          placeholder="DESCRIPTION"
+          name="description"
+          defaultValue={defaultValue.description}
         />
-        <button type="button">Envoyer</button>
+
+        <p className="auth-gcu">
+          <span className="star">*</span> En soumettant ce projet, vous acceptez
+          que celui-ci soit publié sur le site.
+        </p>
+        <button className="post-project-button" type="submit">
+          {children}SOUMETTRE LE PROJET
+        </button>
       </form>
     </section>
   );
 }
 
-export default NewProjectForm;
+export default NextProjectForm;
--- client/src/pages/projectPages/ProjectNew.tsx
+++ client/src/pages/projectPages/ProjectNew.tsx
@@ -1,11 +1,37 @@
 import "./ProjectNew.css";
+import { useNavigate } from "react-router-dom";
 import NewProjectForm from "../../components/NewProjectForm";
 
 function ProjectNew() {
+  const navigate = useNavigate();
+
+  const newProject = {
+    title: "",
+    year: "",
+    technologies: "",
+    description: "",
+  };
+
   return (
     <>
-      <h1>Je suis la page ProjectNew</h1>
-      <NewProjectForm />
+      <NewProjectForm
+        defaultValue={newProject}
+        onSubmit={(projectData) => {
+          fetch(`${import.meta.env.VITE_API_URL}/api/projects`, {
+            method: "post",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(projectData),
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              navigate(`/projects/${data.insertId}`);
+            });
+        }}
+      >
+        Ajouter
+      </NewProjectForm>
     </>
   );
 }
